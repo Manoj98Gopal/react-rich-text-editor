a5a3dabea10a64082a457f20ce3adedc9739eb2d
manoj98gopal <manoj98gopal@gmail.com>
2025-07-03 21:56:28 +0530

    shadcn installed

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h2>Working fine</h2>
+      <Button>Click me</Button>
     </div>
   );
 }
